2c5d384e531914b9916d61ae1e970681ad09d9a4
luchenghan <a0918189209@gmail.com>
2020-07-13 18:19:56 +0800

    insert data into nodedb

---
+++ nodejsmysql/insertdata.js
@@ -0,0 +1,20 @@
+var mysql = require("mysql")
+
+var con = mysql.createConnection({
+    host: "localhost",
+    port:"3306",  
+    user: "root",
+    password: "password",
+    database: "nodedb"
+})
+
+con.connect(function(err){
+    if (err) throw err
+    console.log("Connected!")
+    var sql = "INSERT INTO customers (name, address) VALUES ('Company Inc', 'Highway 37')";
+
+    con.query(sql, function(err, result){
+        if(err) throw err;
+        console.log("1 record inserted")
+    })
+})
