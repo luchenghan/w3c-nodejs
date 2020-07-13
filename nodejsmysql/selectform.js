d87727aef7abbfaabf0df09ce8b0ab970c29ce00
luchenghan <a0918189209@gmail.com>
2020-07-13 18:34:34 +0800

    select from table

---
+++ nodejsmysql/selectform.js
@@ -0,0 +1,20 @@
+var mysql = require("mysql")
+
+var con = mysql.createConnection({
+    host: "localhost",
+    port:"3306",  
+    user: "root",
+    password: "password",
+    database: "nodedb"
+})
+
+con.connect(function(err) {
+    if (err) throw err;
+    con.query("SELECT name, address FROM customers", function (err, result, fields) {
+      if (err) throw err;
+
+      console.log(result[2].name)   // result object
+      console.log(fields);  //fields object
+
+    });
+});
