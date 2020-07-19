f0eb29c0d2bc6b7ce2c0bb5cef301c69f875a6e7
luchenghan <a0918189209@gmail.com>
2020-07-19 08:21:34 +0800

    add eventloop

---
+++ eventloop.js
@@ -0,0 +1,28 @@
+console.log(1)
+
+setTimeout(() => {
+    console.log(2)
+    new Promise(resolve => {
+        console.log(4)
+        resolve()
+    }).then(() => {
+        console.log(5)
+    })
+})
+
+new Promise(resolve => {
+    console.log(7)
+    resolve()
+}).then(() => {
+    console.log(8)
+})
+
+setTimeout(() => {
+    console.log(9)
+    new Promise(resolve => {
+        console.log(11)
+        resolve()
+    }).then(() => {
+        console.log(12)
+    })
+})
